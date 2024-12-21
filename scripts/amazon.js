a93a02d49eed13f95d599c69f19a31a0afc86681
oktavyanha <oktavyanhardiyono@gmail.com>
2024-12-21 08:59:44 +0700

    Add product data script and include it in the HTML file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,3 @@
-// Description: create initial Amazon product data
-const products = [{
-  image : `images/products/athletic-cotton-socks-6-pairs.jpg`,
-  name : `Athletic Cotton Socks 6 Pairs`,
-  rating : {
-    stars : 4.5,
-    count: 87,
-  },
-  priceCents : 1090,
-}, {
-  image : `images/products/intermediate-composite-basketball.jpg`,
-  name : `Intermediate Composite Basketball`,
-  rating : {
-    stars : 4,
-    count: 127,
-  },
-  priceCents : 2095
-}, {
-  image : `images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg`,
-  name : `Adult Plain Cotton T-shirt 2 Pack Teal`,
-  rating : {
-    stars : 4.5,
-    count: 56,
-  },
-  priceCents : 799
-}];
-
 let productHTML = '';
 
 products.forEach((product) => { 
