34bac60289c536a9f67747ce47d38130d80af208
oktavyanha <oktavyanhardiyono@gmail.com>
2024-12-26 17:54:56 +0700

    Implement addToCart function and update cart quantity display

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let productAlreadyInCart;
+    
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      productAlreadyInCart = cartItem;
+    }
+  });
+
+  if (productAlreadyInCart) {
+    productAlreadyInCart.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1  
+    });
+  }
+}
