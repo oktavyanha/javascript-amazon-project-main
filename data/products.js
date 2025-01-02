66fa8f830c9b6e1b8af00f213d8619301f52c440
oktavyanha <oktavyanhardiyono@gmail.com>
2025-01-02 19:12:13 +0700

    convert loadProducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -103,6 +103,34 @@ logThis.call('hello');
 
 export let products = [];
 
+export function loadProductsFetch(){
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productData) => {
+    products = productData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'appliance') {
+        return new Appliance(productDetails);
+
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('Products loaded');
+  });
+
+  return promise;
+};
+
+/*
+loadProductsFetch().then(() => {
+  console.log('Next Step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', () => {
@@ -124,6 +152,7 @@ export function loadProducts(fun) {
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+
 
 
 /*
