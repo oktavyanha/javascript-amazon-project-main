b535dd19cfc7fbbe9124a291ad74423e0a23b323
oktavyanha <oktavyanhardiyono@gmail.com>
2025-01-02 11:29:01 +0700

    load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,14 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
+import { loadProducts } from "../data/products.js";
+
 // import '../data/car.js'
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
+loadProducts(() => { // this code read: use every function below as a parameter
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
