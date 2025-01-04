10590e81d7d2e3476c58eae7626342e37d2aeee0
oktavyanha <oktavyanhardiyono@gmail.com>
2025-01-04 21:24:13 +0700

    make buy again button interactive

--- scripts/orders.js
+++ scripts/orders.js
@@ -2,6 +2,7 @@ import { getProduct, loadProductsFetch } from "../data/products.js";
 import { orders } from "../data/orders.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import formatCurrency from "./utils/money.js";
+import { addToCart } from "../data/cart.js";
 
 
 
@@ -60,7 +61,8 @@ async function loadPage() {
           <div class="product-quantity">
             Quantity: ${productDetails.quantity}
           </div>
-          <button class="buy-again-button button-primary">
+          <button class="buy-again-button button-primary js-buy-again"
+          data-product-id="${product.id}">
             <img class="buy-again-icon" src="images/icons/buy-again.png">
             <span class="buy-again-message">Buy it again</span>
           </button>
@@ -82,6 +84,20 @@ async function loadPage() {
 
   document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
 
+  document.querySelectorAll('.js-buy-again').forEach((button) => {
+    button.addEventListener('click', () => {
+      addToCart(button.dataset.productId);
+
+      button.innerHTML = 'Added';
+
+      setTimeout(() => {
+        button.innerHTML = `
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        `;
+      }, 1000);
+    });
+  });
 }
 
 loadPage();
